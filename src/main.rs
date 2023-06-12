1826b1abbea8d677450696aae29746ffea9f500b
kadoshita <sublimer.me@gmail.com>
2023-06-12 18:55:19 +0900

    Add template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser};
+
+#[derive(Parser, Debug)]
+#[clap(
+    name = env!("CARGO_PKG_NAME"),
+    version = env!("CARGO_PKG_VERSION"),
+    author = env!("CARGO_PKG_AUTHORS"),
+    about = env!("CARGO_PKG_DESCRIPTION"),
+    arg_required_else_help = false,
+    disable_help_subcommand = true
+)]
+struct Args {
+    #[arg(
+        short,
+        long,
+        default_value_t = 1,
+        help = "Number of generate addresses"
+    )]
+    number: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args);
 }
